be35c80825a2a4c9d3eded8d006a388be4405c58
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2016-05-10 14:13:41 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
+export interface DeferredDisposable<T> {
+    using<R>(f: (value: T) => Promise<R>): Promise<R>;
+    map<R>(f: (value: T) => R): DeferredDisposable<R>;
+    and<R>(f: (value: T) => DeferredDisposable<R>): DeferredDisposable<R>;
+}
+
 export function deferredDisposable<T>(init: () => Promise<T>, dispose: (value: T) => Promise<void>):
-    <R>(f: (value: T) => Promise<R>) => Promise<R> {
+    DeferredDisposable<T> {
     
     return null;
 }
