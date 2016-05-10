f85f8d9c751a90d1c4abeabf38bd935779b01357
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2016-05-10 14:07:28 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-export function deferredDisposable<T>(init: () => Promise<T>, dispose: (value: T) => Promise<void>): <R>(value: T) => Promise<R> {
+export function deferredDisposable<T>(init: () => Promise<T>, dispose: (value: T) => Promise<void>):
+    <R>(f: (value: T) => Promise<R>) => Promise<R> {
+    
     return null;
 }
