f5f3bcd6faaafd77562b5262e954399d0cc42375
Sergey Shandar <sergey.shandar@gmail.com>
2016-06-10 03:07:46 -0700

    SyncMem

--- syncmem.ts
+++ syncmem.ts
@@ -1,34 +1,34 @@
-import * as O from "./optimized";
-import * as B from "./bag";
-import * as D from "./dag";
-import * as A from "./array";
-import * as F from "./flatten";
+import * as optimized from "./optimized";
+import * as bag from "./bag";
+import * as dag from "./dag";
+import * as array from "./array";
+import * as flatten from "./flatten";
 
-type GetArray<T> = () => T[];
+export type GetArray<T> = () => T[];
 
-class SyncMem {
+export class SyncMem {
 
     private _map: { [id: string]: GetArray<any> } = {};
 
-    private _dag: D.Dag = new D.Dag();
+    private _dag: dag.Dag = new dag.Dag();
 
-    set<T>(input: B.Bag<T>, getArray: GetArray<T>): void {
+    set<T>(input: bag.Bag<T>, getArray: GetArray<T>): void {
         this._map[input.id] = getArray;
     }
 
-    get<T>(b: B.Bag<T>): GetArray<T> {
+    get<T>(b: bag.Bag<T>): GetArray<T> {
         const id = b.id;
         let result = this._map[id];
         if (result !== undefined) {
             return result;
         }
         const links = this._dag.get(b).array
-            .map(link => link.implementation(<I>(value: O.LinkValue<T, I>) => {
+            .map(link => link.implementation(<I>(value: optimized.LinkValue<T, I>) => {
                 // NOTE: possible optimization:
                 // if (f === flatten.identity) { return nodeFunc; }
                 const f = value.func;
                 const nodeFunc = this._fromNode(value.node);
-                return () => A.ref(nodeFunc()).flatten(f);
+                return () => array.ref(nodeFunc()).flatten(f);
             }));
         result = this._map[id];
         if (result !== undefined) {
@@ -36,22 +36,22 @@ class SyncMem {
         }
 
         // NOTE: possible optimization: if (links.lenght === 1) { newResult = links[0]; }
-        const refLinks = A.ref(links);
+        const refLinks = array.ref(links);
         const newResult = () => refLinks.flatten(f => f());
 
         this._map[b.id] = newResult;
         return newResult;
     }
 
-    private _get<T>(o: O.Bag<T>): GetArray<T> {
+    private _get<T>(o: optimized.Bag<T>): GetArray<T> {
         const id = o.id;
         const links = o.array
-            .map(link => link.implementation(<I>(value: O.LinkValue<T, I>) => {
+            .map(link => link.implementation(<I>(value: optimized.LinkValue<T, I>) => {
                 // NOTE: possible optimization:
                 // if (f === flatten.identity) { return nodeFunc; }
                 const f = value.func;
                 const nodeFunc = this._fromNode(value.node);
-                return () => A.ref(nodeFunc()).flatten(f);
+                return () => array.ref(nodeFunc()).flatten(f);
             }));
         const result = this._map[id];
         if (result !== undefined) {
@@ -59,29 +59,29 @@ class SyncMem {
         }
 
         // NOTE: possible optimization: if (links.lenght === 1) { newResult = links[0]; }
-        const refLinks = A.ref(links);
+        const refLinks = array.ref(links);
         const newResult = () => refLinks.flatten(f => f());
 
         this._map[id] = newResult;
         return newResult;
     }
 
-    private _fromNode<T>(n: O.Node<T>): GetArray<T> {
+    private _fromNode<T>(n: optimized.Node<T>): GetArray<T> {
         const id = n.id;
         const map = this._map;
         const result = map[id];
         if (result !== undefined) {
             return result;
         }
-        const get = <I>(b: O.Bag<I>) => this._get(b);
-        class Visitor implements O.NodeVisitor<T, GetArray<T>> {
+        const get = <I>(b: optimized.Bag<I>) => this._get(b);
+        class Visitor implements optimized.NodeVisitor<T, GetArray<T>> {
 
             input(): GetArray<T> { return () => map[id](); }
 
             one(value: T): GetArray<T> { return () => [value]; }
 
             groupBy(
-                input: O.Bag<T>, toKey: B.KeyFunc<T>, reduce: B.ReduceFunc<T>
+                input: optimized.Bag<T>, toKey: bag.KeyFunc<T>, reduce: bag.ReduceFunc<T>
             ): GetArray<T> {
                 const inputLazyArray = get(input);
                 return () => {
@@ -97,14 +97,14 @@ class SyncMem {
             }
 
             product<A, B>(
-                a: O.Bag<A>, b: O.Bag<B>, func: B.ProductFunc<A, B, T>
+                a: optimized.Bag<A>, b: optimized.Bag<B>, func: bag.ProductFunc<A, B, T>
             ): GetArray<T> {
                 const getA = get(a);
                 const getB = get(b);
                 return () => {
                     // NOTE: possible optimization: we can cache the result of the function.
-                    const aArray = A.ref(getA());
-                    const bArray = A.ref(getB());
+                    const aArray = array.ref(getA());
+                    const bArray = array.ref(getB());
                     return aArray.flatten(av => bArray.flatten(bv => func(av, bv)));
                 };
             }
---
+++ test/syncmem-test.ts
@@ -0,0 +1,31 @@
+import { SyncMem } from "../syncmem";
+import * as bag from "../bag";
+import * as chai from "chai";
+
+chai.should();
+
+describe("syncmem.ts", function() {
+    describe("class SyncMem", function() {
+        it("set()", () => {
+            const syncMem = new SyncMem();
+            const input = bag.input<number>();
+            const f = () => [123];
+            syncMem.set(input, f);
+            syncMem.get(input).should.equal(f);
+            syncMem.get(input.disjointUnion(bag.one(5)))().should.deep.equal([123, 5]);
+            syncMem.get(input.product(bag.one([1, 2, 3]).flatten(x => x), (a, b) => [a * b]))()
+                .should.deep.equal([123, 246, 369]);
+        })
+        it("get()", () => {
+            const syncMem = new SyncMem();
+            const r = bag.one("Hello world!");
+            syncMem.get(r)().should.deep.equal(["Hello world!"]);
+            syncMem.get(r.flatten(x => [x, x]).reduce((a, b) => a + b))().should.deep
+                .equal(["Hello world!Hello world!"]);
+            const input = bag.input<string>();
+            const x = syncMem.get(input);
+            syncMem.set(input, () => ["abc", "def"]);
+            x().should.deep.equal(["abc", "def"]);
+        })
+    })
+})
