287b5b73afcc71f62c4c0029eac8cf4500a8b771
Sergey Shandar <sergey.shandar@gmail.com>
2016-07-25 12:00:52 -0700

    comments

--- demo/ui.ts
+++ demo/ui.ts
@@ -1,22 +1,33 @@
 import * as bag from "../bag";
 import * as syncmem from "../syncmem";
 
+// Data Structure
+
 class Account {
-    constructor(public name: string, public total: number) {
+    constructor(
+        public name: string,
+        public total: number) {
     }
 }
+
+// Logic
 
 const accountsBag = bag.input<Account>();
 
 const sumBug = accountsBag.map(account => account.total).reduce((a, b) => a + b);
+
+// Data for synchronous back-end
 
 const accountsArray = [
     new Account("First Account", 123.00),
     new Account("Second Account", 123.45),
 ];
 
+// Using SyncMem back-end.
+
 const engine = new syncmem.SyncMem();
 
+// associate local data and generic logic.
 engine.set(accountsBag, () => accountsArray);
 
 export const total = engine.get(sumBug)()[0];
